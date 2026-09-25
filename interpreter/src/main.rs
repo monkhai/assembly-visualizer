75ee480e785cc847d9c84a06fe074ac4c1c7cadd
Yohai Wiener <yohaiwiener@gmail.com>
2026-09-24 21:09:27 -0500

    starting to think about instruction execution

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("hello world");
+    /*
+     * initialize code snippet
+     * read through the code, splitting through InstructionExpressions basically.
+     * then you have an evaluator per instruction type, that updates the data structure and emits the new snapshot
+     */
 }
